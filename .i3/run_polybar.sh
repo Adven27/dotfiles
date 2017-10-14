77318b8e54ffc4f483729eef5dfbb9be9df471ab
adven27 <n.toropanov@gmail.com>
2017-10-14 21:46:27 +0300

    smallfixes

--- .i3/run_polybar.sh
+++ .i3/run_polybar.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-killall polybar
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null;do sleep 1; done
+
 polybar bar1 &
 polybar bar0 &
+#polybar top &
+
+echo "Polybar launched..."
